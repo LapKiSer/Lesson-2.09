c07d1e408ddca0d84ec24cfecc865c2576b3f624
Lapkovskiy Kirill <lapkovskiykirill@gmail.com>
2023-04-11 17:13:04 +0300

    Initial training

---
+++ ColorizedApp/ViewControllers/EmptyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EmptyViewController.swift
+//  ColorizedApp2
+//
+//  Created by Кирилл Лапковский on 11.04.2023.
+//
+
+import UIKit
+
+final class EmptyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
